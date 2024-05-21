e5b671304b499a4bee04f5263209aa87eafc747d
Nam Tinnakorn RMU <tinnakorn.pei@rmu.ac.th>
2024-05-21 11:38:06 +0700

    change input component style

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -20,7 +20,7 @@ const Inputs: React.FunctionComponent<IInputsProps> = (props) => {
       <input
         type={type}
         id={name}
-        className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+        className="py-3 px-4 block w-full border-0 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
         placeholder={placeholder}
         aria-describedby="hs-input-helper-text"
       />
