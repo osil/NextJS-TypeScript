d0f80ef468ab83ba012865556f7cc426710250c8
Nam Tinnakorn RMU <tinnakorn.pei@rmu.ac.th>
2024-05-21 10:41:29 +0700

    Change to TypeScript

---
+++ src/components/Dataset.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useEffect } from "react";
+import { useAppSelector, useAppDispatch } from "@/lib/hooks";
+import { fetchDataset } from "@/lib/features/dataset/datasetSlice";
+
+function Dataset() {
+  const dispatch = useAppDispatch();
+  const dataSets = useAppSelector((state) => state.dataset.dataSets);
+
+  useEffect(() => {
+    dispatch(fetchDataset());
+  }, [dispatch]);
+
+  return (
+    <>
+      <div>Dataset</div>
+      {dataSets.map((dataset: any) => (
+        <p key={dataset.id}> {dataset.datasetname}</p>
+      ))}
+    </>
+  );
+}
+
+export default Dataset;
