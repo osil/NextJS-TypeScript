b2411a27ddceb0ab2392a7eec2b23e3cb8b4fcfc
Nam Tinnakorn RMU <tinnakorn.pei@rmu.ac.th>
2024-05-23 09:50:14 +0700

    add zod confirm password

--- src/app/(Auth)/register/page.tsx
+++ src/app/(Auth)/register/page.tsx
@@ -6,13 +6,25 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import Inputs from "@/components/Inputs";
 
-const FormSchema = z.object({
-  username: z.string().min(2, "กรุณาระบุชื่อผู้ใช้"),
-  password: z
-    .string()
-    .min(6, "รหัสผ่าน 6 ตัวขึ้นไป")
-    .max(20, "รหัสผ่านไม่เกิน 20 ตัว"),
-});
+const FormSchema = z
+  .object({
+    fullname: z.string().min(2, "กรุณาระบุชื่อผู้ใช้"),
+    email: z.string().email("กรุณากรอกอีเมล"),
+    password: z
+      .string()
+      .min(6, "รหัสผ่าน 6 ตัวขึ้นไป")
+      .max(20, "รหัสผ่านไม่เกิน 20 ตัว"),
+    confirmPassword: z.string(),
+  })
+  .refine(
+    (values) => {
+      return values.password === values.confirmPassword;
+    },
+    {
+      message: "Passwords must match!",
+      path: ["confirmPassword"],
+    }
+  );
 type FormSchemaType = z.infer<typeof FormSchema>;
 
 type Props = {};
@@ -27,19 +39,27 @@ const Register = (props: Props) => {
     resolver: zodResolver(FormSchema),
   });
   const onSubmit: SubmitHandler<FormSchemaType> = (data) => console.log(data);
-  console.log(errors);
 
   return (
     <>
       <div>Register</div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Inputs
-          name="username"
+          name="fullname"
           type="text"
-          placeholder="Username"
-          label="Username"
+          placeholder="fullname"
+          label="fullname"
           register={register}
-          error={errors?.username?.message}
+          error={errors?.fullname?.message}
+          disable={isSubmitting}
+        />
+        <Inputs
+          name="email"
+          type="text"
+          placeholder="email"
+          label="email"
+          register={register}
+          error={errors?.email?.message}
           disable={isSubmitting}
         />
         <Inputs
@@ -49,6 +69,15 @@ const Register = (props: Props) => {
           label="Password"
           register={register}
           error={errors?.password?.message}
+          disable={isSubmitting}
+        />
+        <Inputs
+          name="confirmPassword"
+          type="password"
+          placeholder="*******"
+          label="Confirm Password"
+          register={register}
+          error={errors?.confirmPassword?.message}
           disable={isSubmitting}
         />
         <button>Submit</button>
